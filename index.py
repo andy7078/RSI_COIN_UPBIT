c381412a8fdabfc4465a9759696fbe4ac049c647
andy7078 <85816070+andy7078@users.noreply.github.com>
2021-10-04 14:47:14 +0900

    Add files via upload

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ access = "Mx7GdxxulfojBwPH8ULX45RN9f6tXuKUraBvVeJD"          # 본인 값으로 
 secret = "OXuNQOTeOMLtGwdhrDHxJSQF5TigZFTPT7AzpdZr"          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 KRWbalance = upbit.get_balance("KRW")
-buy_price = 100000
+buy_price = 50000
 coinlist = pyupbit.get_tickers('KRW')
 STARTLIST = []
 RSILIST = []
@@ -46,31 +46,32 @@ def MAKELIST():
 def MAKEORDER():
     while True:
         for KRWcoin in coinlist:
-            if KRWcoin in ORDERLIST:
-                NULL=0
-            else:
-                coinnum = coinlist.index(KRWcoin)
-                df = pyupbit.get_ohlcv(KRWcoin, interval="minute1")
-                print(KRWcoin)
-                #KRWbalance = upbit.get_balance("KRW")
-                def rsi(ohlc: df, period: int = 14):
-                    ohlc["close"] = ohlc["close"]
-                    delta = ohlc["close"].diff()
+            if KRWbalance > buy_price
+                if KRWcoin in ORDERLIST:
+                    NULL=0
+                else:
+                    coinnum = coinlist.index(KRWcoin)
+                    df = pyupbit.get_ohlcv(KRWcoin, interval="minute1")
+                    print(KRWcoin)
+                    #KRWbalance = upbit.get_balance("KRW")
+                    def rsi(ohlc: df, period: int = 14):
+                        ohlc["close"] = ohlc["close"]
+                        delta = ohlc["close"].diff()
 
-                    up, down = delta.copy(), delta.copy()
-                    up[up < 0] = 0
-                    down[down > 0] = 0
+                        up, down = delta.copy(), delta.copy()
+                        up[up < 0] = 0
+                        down[down > 0] = 0
 
-                    _gain = up.ewm(com=(period - 1), min_periods=period).mean()
-                    _loss = down.abs().ewm(com=(period - 1), min_periods=period).mean()
+                        _gain = up.ewm(com=(period - 1), min_periods=period).mean()
+                        _loss = down.abs().ewm(com=(period - 1), min_periods=period).mean()
 
-                    RS = _gain / _loss
-                    return pd.Series(100 - (100 / (1 + RS)), name="RSI")
-                    rsi = rsi(df, 14).iloc[-1]
-                if RSILIST[coinnum] + 10 <= rsi:
-                    print(upbit.buy_market_order(KRWcoin, buy_price))
-                    ORDERLIST.append(KRWcoin)
-                    PRICELIST.append(pyupbit.get_current_price(KRWcoin))
+                        RS = _gain / _loss
+                        return pd.Series(100 - (100 / (1 + RS)), name="RSI")
+                        rsi = rsi(df, 14).iloc[-1]
+                    if RSILIST[coinnum] + 10 <= rsi:
+                        print(upbit.buy_market_order(KRWcoin, buy_price))
+                        ORDERLIST.append(KRWcoin)
+                        PRICELIST.append(pyupbit.get_current_price(KRWcoin))
                 
         if KRWcoin == "KRW-XEC":
             break
@@ -82,12 +83,15 @@ def DEL():
     RSILIST.clear()
                 
 # 매일 특정 HH:MM 및 다음 HH:MM:SS에 작업 실행
-schedule.every().day.at("06:55:40").do(MAKELIST)
-schedule.every().day.at("06:56:40").do(MAKEORDER)
-schedule.every().day.at("06:57:40").do(MAKEORDER)
-schedule.every().day.at("06:58:40").do(MAKEORDER)
-schedule.every().day.at("06:59:40").do(DEL)
-        
+schedule.every().hour.at(":59").do(MAKELIST)
+schedule.every().hour.at(":00").do(MAKEORDER)
+schedule.every().hour.at(":01").do(MAKEORDER)
+schedule.every().hour.at(":02").do(MAKEORDER)
+schedule.every().hour.at(":03").do(MAKEORDER)
+schedule.every().hour.at(":04").do(MAKEORDER)
+schedule.every().hour.at(":05").do(MAKEORDER)
+schedule.every().hour.at(":06").do(DEL)
+
 
 
 
